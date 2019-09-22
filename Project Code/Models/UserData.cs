6d77afd5acf920dcd34b1e1cf66f25bd9b161665
Wesley Karle <47843118+wesleykarle@users.noreply.github.com>
2019-09-21 19:55:47 -0700

    Add files via upload

---
+++ Project Code/Models/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4ALLweb.Models
+{
+    public class UserData
+    {
+        [Key]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public decimal UserName { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
